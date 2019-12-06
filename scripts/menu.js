6086a7f720881d20d56debd956af18d4dc220854
stg40 <stg40stg@gmail.com>
2019-12-06 15:38:00 +0300

    some different changes

---
+++ scripts/menu.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var popup = document.getElementById("cart-button-pop-up");
+    popup.classList.toggle("cart-button-menu");
+    var popuptext = document.getElementById("cart-button-pop-up-table-hidden");
+    popuptext.classList.toggle("cart-button-pop-up-table");
+}
